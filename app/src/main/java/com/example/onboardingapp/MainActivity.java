99e316de294cc23a045e4b269a9b69202246867d
root <root@localhost.localdomain>
2019-08-07 13:43:16 +0100

    Referential Purpose

---
+++ app/src/main/java/com/example/onboardingapp/MainActivity.java
@@ -0,0 +1,176 @@
+package com.example.onboardingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mSlideViewPager;
+    private LinearLayout mDotLayout;
+
+    private Button mNextBtn;
+    private Button mPrevBtn;
+
+    private TextView[] mDots;
+    private SliderAdapter sliderAdapter;
+
+    private int mCurrentPage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSlideViewPager = findViewById(R.id.view_pager);
+        mDotLayout = findViewById(R.id.dot_layout);
+
+        mNextBtn = findViewById(R.id.button_next);
+        mPrevBtn = findViewById(R.id.button_previous);
+
+//       we initialize the created SliderAdapter class and it to a referral variable 'sliderAdapter'
+        sliderAdapter = new SliderAdapter(this);
+
+//        we then set the adapter to our viewPager
+        mSlideViewPager.setAdapter(sliderAdapter);
+
+//        we then call the addDotsIndicator method in the onCreate
+        addDotsIndicator(0);
+
+//        we then set the onPageChangeListener to our onCreate
+        mSlideViewPager.addOnPageChangeListener(viewListener);
+
+//        we set onClickListeners to our buttons
+        mNextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                increases the array position of the slide by 1 when the next button is clicked to navigate to the next slide
+                mSlideViewPager.setCurrentItem(mCurrentPage +1);
+            }
+        });
+
+        mPrevBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                reduces the array position of the slide by 1 when the prev button is clicked to navigate to the previous slide
+                mSlideViewPager.setCurrentItem(mCurrentPage -1 );
+            }
+        });
+
+    }
+
+//    this method creates only the number of dots needed according to our array size
+    public void addDotsIndicator(int position){
+
+        mDots = new TextView[3];
+
+//        we remove all the dot views from our layout else multiple dots will be created
+        mDotLayout.removeAllViews();
+
+//        we define loop for every textView in the mDots array less than its total size
+        for (int i = 0; i < mDots.length; i++){
+
+//            we create a new tv and cast it to this activity
+            mDots[i] = new TextView(this);
+
+//             we then put our dots using a html text
+            mDots[i].setText(Html.fromHtml("&#8226;"));
+
+//             we set the textSize to 35
+            mDots[i].setTextSize(35);
+
+//            we set the color of the text to a color from our color res
+            mDots[i].setTextColor(getResources().getColor(R.color.colorTransparentWhite));
+
+//             we set the properties attached to the mDots array to the layout in the xml
+            mDotLayout.addView(mDots[i]);
+
+        }
+
+//        we check if the length of the mDots array is greater than zero (which is definitely true)
+        if (mDots.length > 0){
+
+//            we set the color of the current position to pure white
+            mDots[position].setTextColor(getResources().getColor(R.color.colorWhite));
+        }
+
+    }
+
+//    we set an OnPageListener to add effects to indicate when the slide is changed
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+//    this method is to indicate the array position of the selected slide
+    @Override
+    public void onPageSelected(int position) {
+//        we pass the current i in the addDotsIndicator to be the position
+        addDotsIndicator(position);
+
+//        we set the selected page position to the mCurrentPage variable
+        mCurrentPage = position;
+
+//        we check if the position of the current page is 0 meaning that the user is on the first page
+        if (position == 0){
+
+//            we enable the nextButton for navigation to the next slide
+            mNextBtn.setEnabled(true);
+//            we disable the prevButton as there is no previous slide
+            mPrevBtn.setEnabled(false);
+//            we make the prevButton invisible to prevent the user from clicking it
+            mPrevBtn.setVisibility(View.INVISIBLE);
+
+//            we then set the text of the nextButton to 'Next'
+            mNextBtn.setText("Next");
+//            and set the text of the prevButton to empty
+            mPrevBtn.setText("");
+
+        }
+//        we check if the position of the current page is equal to total array size (meaning it is the last slide)
+//        we put (-1) because array positioning starts from 0
+        else if (position == mDots.length - 1){
+
+//            we enable the nextButton for navigation to the next function
+            mNextBtn.setEnabled(true);
+//            we enable the prevButton as there are now previous slide
+            mPrevBtn.setEnabled(true);
+//            we make the prevButton visible
+            mPrevBtn.setVisibility(View.VISIBLE);
+
+//            we then set the text of the nextButton to 'Finish' as there is no more next slide
+            mNextBtn.setText("Finish");
+//            and set the text of the prevButton to 'Prev' to navigate the previous slides
+            mPrevBtn.setText("Prev");
+        }
+        else{
+
+//            we enable the nextButton for navigation to the next function
+            mNextBtn.setEnabled(true);
+//            we enable the prevButton for navigation to the previous function
+            mPrevBtn.setEnabled(true);
+//            we make the prevButton visible
+            mPrevBtn.setVisibility(View.VISIBLE);
+
+//            we then set the text of the nextButton to 'Next' to navigate the next slides
+            mNextBtn.setText("Next");
+//            and set the text of the prevButton to 'Prev' to navigate the previous slides
+            mPrevBtn.setText("Prev");
+        }
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+};
+}
---
+++ app/src/main/java/com/example/onboardingapp/SliderAdapter.java
@@ -0,0 +1,98 @@
+package com.example.onboardingapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+//  we extend the PagerAdapter to the SliderAdapter class
+public class SliderAdapter extends PagerAdapter {
+
+    Context context;
+    LayoutInflater layoutInflater;
+
+//    we create a constructor of the class and pass the context of this class in order to enable us refer this class context in another class
+    public SliderAdapter (Context context){
+        this.context = context;
+    }
+
+//    we create an array to contain the icons of all the slides
+    public int[] slide_images = {
+
+            R.drawable.img_eat,
+            R.drawable.img_sleep,
+            R.drawable.img_code
+    };
+
+//     we create an array to contain the headings of all the slides
+    public String[] slide_headings = {
+
+            "EAT" ,
+            "SLEEP" ,
+            "CODE"
+    };
+
+//     we create an array to contain the descriptions of all the slides
+
+    public String[] slide_description = {
+
+            "this is a dummy description, another dummy sentence, then the third dummy statement, more dummy statement" +
+                    "final dummy statement",
+            "this is a dummy description, another dummy sentence, then the third dummy statement, more dummy statement" +
+                    "final dummy statement",
+            "this is a dummy description, another dummy sentence, then the third dummy statement, more dummy statement" +
+                    "final dummy statement"
+    };
+
+//    we return the number of slide headings in our headings array
+    @Override
+    public int getCount() {
+        return slide_headings.length;
+    }
+
+
+//    we indicate that our main view for the sliders will contain a relative layout
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == (RelativeLayout) object;
+    }
+
+
+//    this method inflates all the data/ slide effects into this adapter
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slide_layout,container, false);
+
+//        we create view vairables and cast them to the slide_layout
+        ImageView slideImageView = view.findViewById(R.id.slide_image);
+        TextView slideHeading = view.findViewById(R.id.slide_heading);
+        TextView slideDescription = view.findViewById(R.id.slide_desc);
+
+//        we set the cast image to the images array and pass the current position of the current slide for the needed details to be cast to it and selected automatically
+        slideImageView.setImageResource(slide_images[position]);
+        slideHeading.setText(slide_headings[position]);
+        slideDescription.setText(slide_description[position]);
+
+//        we add the view of the slide_layout to the container of this method
+        container.addView(view);
+
+        return view;
+    }
+
+//    this method ensures that once we get to the last slide we are prevented from getting an error
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+
+//        we set that in this method the object which is the relative layout is removed
+        container.removeView((RelativeLayout)object);
+    }
+}
